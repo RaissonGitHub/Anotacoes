955762e81f1b5549f8f6d28d286387603c299aca
RaissonGitHub <raissonarbustepaludo@gmail.com>
2023-07-30 19:59:31 -0300

    Adicionar anotacoes 003

---
+++ Node.js/Codigos_e_Anotacoes/003/003c.js
@@ -0,0 +1,17 @@
+let ferramentas = ['martelo', 'pá', 'alicate'] // criando lista
+let moveis = ['cadeira', 'mesa', 'porta'] // criando lista
+
+//exportando mais coisas coloque em forma de objeto
+//Shortcut key
+module.exports = { 
+    ferramentas,
+    moveis
+}
+
+//Pode ser feito colocando com propriedades
+/*
+module.exports = { 
+    colecao_ferramentas: ferramentas,
+    colecao_moveis: moveis
+}
+*/
